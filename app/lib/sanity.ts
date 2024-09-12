c5b2599b3112efd3741c92eb6f4c553906776246
manyamOpenthrive <manyam.c@openthrive.com>
2024-09-12 19:29:59 +0530

    any to Image type

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,14 +1,17 @@
 import { createClient } from "next-sanity";
-import imageUrlBuilder from '@sanity/image-url'
+import imageUrlBuilder from "@sanity/image-url";
+import type { Image } from "sanity"; // Importing the Image type from Sanity
+
 export const client = createClient({
-    apiVersion: '2022-03-07',
-    dataset: 'production',
-    projectId : '9gmkgxkq',
-    useCdn: false,
-})
+  apiVersion: "2022-03-07",
+  dataset: "production",
+  projectId: "9gmkgxkq",
+  useCdn: false,
+});
 
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source: any){
-    return builder.image(source)
+// Specify the type for the source parameter
+export function urlFor(source: Image) {
+  return builder.image(source);
 }
