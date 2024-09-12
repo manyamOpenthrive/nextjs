a34a8fa251590b66c351e6e736500ee2cc54cab8
manyamOpenthrive <manyam.c@openthrive.com>
2024-09-12 19:41:07 +0530

    any to Image type

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,21 +1,21 @@
 import type { Image } from "sanity"; // If not available directly, you might need to define or import the correct type.
 import type { PortableTextBlock } from "@portabletext/types"; // Import for rich text content type.
 
-export interface SimpleBlogCard {
+export interface simpleBlogCard {
   title: string;
   smallDescription: string;
   currentSlug: string;
   titleImage: Image; // Assuming titleImage is a Sanity image object.
 }
 
-export interface Card {
+export interface card {
   imageurl: Image; // Assuming imageurl is a Sanity image object.
   title: string;
   shortdesc: string;
   slug: string;
 }
 
-export interface FullBlog {
+export interface fullblog {
   currentSlug: string;
   title: string;
   content: PortableTextBlock[]; // Assuming content is an array of Portable Text Blocks.
